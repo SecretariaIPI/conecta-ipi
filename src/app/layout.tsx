9a740139f8c90f0d94a2e49c17e795ea32776a3a
SecretariaIPI <secretaria@ipicascavel.com.br>
2026-06-11 21:29:53 +0000

    Organização: Tela de boas-vindas movida para pasta dedicada com redirecionamento na raiz

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 'use client'
+declare module '*.css'
 import './globals.css'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from 'next/navigation';
+
+export default function RootPage() {
+  // Redireciona a URL raiz para a sua pasta organizada
+  redirect('/paginaboasvindas');
+}
